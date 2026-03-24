a19d8982128e50e38eaa8757a8a92c05aa600cf7
Carlos Estrada <cestrada_chacon@outlook.com>
2026-03-23 22:19:54 -0600

    fix camera

--- app/Views/packages/assign/index.php
+++ app/Views/packages/assign/index.php
@@ -282,7 +282,7 @@
                     .finally(() => {
                         setTimeout(() => {
                             scanning = true;
-                        }, 800);
+                        }, 700);
                     });
             }
         );
